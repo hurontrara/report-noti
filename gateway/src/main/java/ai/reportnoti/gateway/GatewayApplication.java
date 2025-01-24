ebce9b68a2f397a9fc9fe54e664ca0bca6d08d79
sunbin <csb7386@gmail.com>
2025-01-24 13:51:07 +0900

    [REP-21] 프로젝트 초기화
    - Eureka Server, Client 설정
    - dockerfile, docker-compose 설정

--- gateway/src/main/java/ai/reportnoti/gateway/GatewayApplication.java
+++ gateway/src/main/java/ai/reportnoti/gateway/GatewayApplication.java
@@ -2,11 +2,14 @@ package ai.reportnoti.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
